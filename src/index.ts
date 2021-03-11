8ea211cfe9aa31a677a402345a1fb9f43adef601
song <as1142017128@gmail.com>
2021-03-12 00:03:24 +0800

    贪吃蛇项目

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import './style/index.less';
+
+// import Food from "./moduls/Food";
+//
+// import ScorePanel from "./moduls/ScorePanel";
+//
+// import Snake from "./moduls/Snake";
+
+import GameControl from "./moduls/GameControl";
+
+new GameControl();
+
+// const food = new Food();
+// console.log(food.X, food.Y);
+// food.change();
+// console.log(food.X, food.Y);
